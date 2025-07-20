e8f3bee1f32a4a75d6d42aa2c23cd42ed1ecc132
Beyang Liu <beyang@sourcegraph.com>
2025-07-19 17:35:43 -0700

    Initial Commit

---
+++ MenuPlayer/MenuPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuPlayerApp.swift
+//  MenuPlayer
+//
+//  Created by Beyang Liu on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MenuPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
